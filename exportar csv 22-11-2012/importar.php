bef2b9676b190c206e5b0f5fc1e42385648cbfde
Pep Sancho Redondo <pep3689@gmail.com>
2012-11-24 02:58:19 +0100

    NOva actualitzacio del modul exportar csv. El canvi reacu en el archiu importar.php. He aconseguit importar l'any a la base de dades segons si esta creat o no. El proxim pas es fer la mateixa funcio per a les diferents taules de la bbdd. He de dir que aquest algoritme me esta portant moltes dificultats pero una vegada realitzat quedara abatida una gran part del projecte. Aquest actualitzacio correspon al dijous dia 22.

---
+++ exportar csv 22-11-2012/importar.php
@@ -0,0 +1,224 @@
+
+
+<?php
+
+require_once ("conexion/header.php");
+
+$fila = 0;
+
+if (($gestor = fopen("reporte.csv", "r")) !== FALSE) 
+{
+
+
+echo "<table width='600' cellpadding='5' cellspacing='5' border='1'>";
+echo "<td> id </td>";
+echo "<td> año </td>";
+echo "<td> Nombre del curso </td>";
+echo "<td> Acronimo </td>";
+echo "<td> Nombre de la clase </td>";
+echo "<td> Codigo </td>";
+echo "<td> Horas totales </td>";
+echo "<td> Nombre del grupo </td>";
+echo "<td> Horas semanales </td>";
+
+    while (($datos = fgetcsv($gestor, 1000, ";")) !== FALSE) 
+	{
+	
+	
+		echo "<tr>";
+        $numero = count($datos);
+        //echo "<p> $numero campos en la línea $fila: <br /></p>\n";
+        $fila++;
+        for ($c=0; $c < $numero; $c++) 
+		{
+			if( $fila != 1)
+			{
+				echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$datos[$c] . "</font></td>";
+				
+				if($c == 1)
+				{	
+					
+					
+					$anysnocreats = "SELECT id as idany, any as any from any_academic where any = '$datos[$c]'";
+					$result = mysql_query($anysnocreats);
+
+						//echo $datos[$c] . "</br>";
+						if(mysql_num_rows($result) < 1)
+						{
+							//----------------------------------------------Faig l'insert - imprimeixc per pantalla el resultat - execut l'insert
+							$insert = "INSERT INTO any_academic(any) VALUES ('$datos[$c]');";
+							echo $insert."<br />";
+							$runInsert = mysql_query($insert) or die(mysql_error()." Error: No se pudo registar la precarga");
+							
+							
+							//------------------------------------Seleccion l'id del nou any introduit - el guard dins una variable - i imprimeixc per pantalla el resultat
+							$anycreat = "Select id from any_academic where any = '$datos[$c]'";
+							$resultid = mysql_query($anycreat);
+							while($row = mysql_fetch_array($resultid))
+							{
+								$idany = $row["id"];
+								//echo "año creado con identificador" . $idany . "</br>";
+							}
+							
+						}
+						else
+						{
+							$anyexistent = "Select id from any_academic";
+							$idexistent = mysql_query($anyexistent);
+							while($row = mysql_fetch_array($idexistent))
+							{
+								$idany = $row["id"];
+								//echo "Año existente con identificador" . $idany . "</br>";
+								
+							}
+							
+						}
+
+				}
+				else if($c == 2)
+				{
+					$cursosnocreats = "SELECT id as idcurs, id_any, nom, acronim from cursos where nom = $datos[$c]";
+					$resultcursos = mysql_query($cursosnocreats);
+
+						//echo $datos[$c] . "</br>";
+						
+						$acronim = $datos[$c + 1];
+						
+						if(mysql_num_rows($resultcursos) < 1)
+						{
+							$insert = "INSERT INTO cursos(id_any, nom, acronim) VALUES ( '$idany', '$datos[$c]', '$acronim');";
+							echo $insert."<br />";
+							$runInsert = mysql_query($insert) or die(mysql_error()." Error: No se pudo registar la precarga");
+							
+							
+							//------------------------------------Seleccion l'id del nou any introduit - el guard dins una variable - i imprimeixc per pantalla el resultat
+							$curscreat = "Select id from cursos where nom = '$datos[$c]'";
+							$resultid = mysql_query($curscreat);
+							while($row = mysql_fetch_array($resultid))
+							{
+								$idcurs = $row["id"];
+								echo "curso creado con identificador" . $idcurs . "</br>";
+							}
+							
+						}
+						else
+						{
+							$cursexistent = "Select id from cursos";
+							$idexistent = mysql_query($cursexistent);
+							while($row = mysql_fetch_array($idexistent))
+							{
+								$idcurs = $row["id"];
+								echo "Curso existente con identificador" . $idcurs . "</br>";
+								
+							}
+							
+						}
+					
+				}
+				/*if ($c == 8 && $fila == 2)
+				{
+					echo $datos[$c] . "<br />\n";
+				}*/
+			}
+        }
+		echo "<tr>";
+		
+    }
+    fclose($gestor);
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- -------------------------------------------------PROVES -----------------------------------------------------------------------------------------
+<?php
+/*
+$fichero = fopen("lista.csv","r")or die("No se consigue el archivo");
+while (!feof($fichero)){
+	$campo = fgetcsv($fichero,4096,";");
+	$hay = count($campo);
+	$insert = "INSERT INTO any_academic(any) VALUES ('$campo[1]');";
+	//echo $insert."<br />";
+	$runInsert = mysql_query($insert,$conexion) or die(mysql_error()." Error: No se pudo registar la precarga");
+}
+fclose($fichero);
+*/
+?>
+
+<?php
+
+/*$fp = fopen ( "reporte.csv" , "r" );
+
+echo "<table width='600' cellpadding='5' cellspacing='5' border='1'>";
+
+
+while (( $data = fgetcsv ($fp,1000,";")) !== FALSE ){
+ 
+ 
+    $i = 0;
+	$numero = count($data);
+
+	
+	echo "<tr>";
+
+	for ($c=0; $c < $numero; $c++) {
+
+		
+			echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$data[$c] . "</font></td>";
+       
+		$i++;
+		
+		
+    }
+	echo "</tr>";
+	
+}
+fclose ( $fp );
+*/
+?>
+
+
+
+<?php
+/*
+$fp = fopen ( "reporte.csv" , "r" );
+
+echo "<table width='600' cellpadding='5' cellspacing='5' border='1'>";
+
+
+while (( $data = fgetcsv ($fp,1000,";")) !== FALSE ){
+ 
+	//echo $data[0]. "</br>";
+    $i = 0;
+	echo "<tr>";
+    foreach($data as $row) {
+
+		
+         //echo "Campo $i: $row<br />";
+        // Muestra todos los campos de la fila actual
+		echo "<td width=\"25%\"><font face=\"verdana\">" . 
+			$row . "</font></td>";
+        $i++ ;
+		
+
+		
+    }
+	echo "</tr>";
+ 
+}
+fclose ( $fp );
+*/
+?>
+
+
---
+++ exportar csv 22-11-2012/plantillas/info_mini.php
@@ -0,0 +1,5 @@
+<div class = "info_mini">
+	<span class = "texto_infomini">
+		Datos de la empresa
+	</span>
+</div>
